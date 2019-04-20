22916fb2338341a78be98f5189153535a1a74af6
Filip <kfilip94@gmail.com>
2019-04-20 20:14:33 +0200

    Add server side data validation using struct - validation library for Clojure(Script)

--- project.clj
+++ project.clj
@@ -8,7 +8,8 @@
                  [org.clojure/java.jdbc "0.3.0-alpha5"]
                  [mysql/mysql-connector-java "5.1.6"]
                  [de.ubercode.clostache/clostache "1.4.0"]
-                 [ring-basic-authentication "1.0.5"]]
+                 [ring-basic-authentication "1.0.5"]
+                 [funcool/struct "1.3.0"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler clojurewebapplication.core/-main}
   :profiles
--- src/clojurewebapplication/domain/meal.clj
+++ src/clojurewebapplication/domain/meal.clj
@@ -1,7 +1,8 @@
 (ns clojurewebapplication.domain.meal
   (:refer-clojure :exclude [get])
   (:require [clojure.java.jdbc :as jdbc]
-            [clojure.java.jdbc.sql :as sql]))
+            [clojure.java.jdbc.sql :as sql]
+            [struct.core :as st]))
 
 (def mysql-db {
                :subprotocol "mysql"
@@ -10,6 +11,25 @@
                :password ""
                :zeroDateTimeBehaviour "convertToNull"
                })
+
+(def schemeUpdate
+  {:meal_id [st/required st/number-str]
+   :meal_name [st/required st/string [st/max-count 60]]
+   :kcal_100g [st/required st/number-str st/positive [st/in-range 0 10000]]
+   :proteins_100g [st/required st/number-str st/positive [st/in-range 0 10000]]
+   :carbs_100g [st/required st/number-str st/positive [st/in-range 0 10000]]
+   :fats_100g [st/required st/number-str st/positive [st/in-range 0 10000]]
+   :meal_type [st/required st/number-str st/positive [st/in-range 0 10000]]
+   })
+
+(def schemeInsert
+  {:meal_name [st/required st/string [st/max-count 60]]
+   :kcal_100g [st/required st/number-str st/positive [st/in-range 0 10000]]
+   :proteins_100g [st/required st/number-str st/positive [st/in-range 0 10000]]
+   :carbs_100g [st/required st/number-str st/positive [st/in-range 0 10000]]
+   :fats_100g [st/required st/number-str st/positive [st/in-range 0 10000]]
+   :meal_type [st/required st/number-str st/positive [st/in-range 0 10000]]
+   })
 
 (defn allMeals []
   (jdbc/query mysql-db
@@ -28,8 +48,9 @@
                  ["DELETE FROM meal WHERE meal_id = ?" id]))
 
 (defn update [id params]
-  (jdbc/update! mysql-db :meal params (sql/where {:meal_id id})))
+  (if (st/valid? params schemeUpdate) (jdbc/update! mysql-db :meal params (sql/where {:meal_id id})) (throw (Exception. "Validation failed")))
+  )
 
-(defn insertMeal
-  [params]
-  (jdbc/insert! mysql-db :meal params))
+(defn insertMeal [params]
+  (if (st/valid? params schemeInsert) (jdbc/insert! mysql-db :meal params) (throw (Exception. "Validation failed")))
+  )
